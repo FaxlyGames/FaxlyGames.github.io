48ad58f13c471129070238998550375781cc1db4
FaxlyGames <faxlygames@gmail.com>
2026-05-28 15:57:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
-function toggleMenu() {
-    document.getElementById("menu").classList.toggle("show");
+const menu =
+document.getElementById("fullscreenMenu");
+
+const hero =
+document.getElementById("inicio");
+
+/* ===================================
+   ABRIR MENU
+=================================== */
+
+function abrirMenu(){
+
+  menu.classList.add("active");
 }
 
-function showSection(id) {
-    let sections = document.querySelectorAll("section");
-    sections.forEach(section => section.classList.remove("active"));
-    document.getElementById(id).classList.add("active");
-    document.getElementById("menu").classList.remove("show");
+/* ===================================
+   CERRAR MENU
+=================================== */
+
+function cerrarMenu(){
+
+  menu.classList.remove("active");
+}
+
+/* ===================================
+   CAMBIAR SECCION
+=================================== */
+
+function mostrarSeccion(id){
+
+  /* HERO DESAPARECE */
+
+  hero.classList.remove("active-hero");
+
+  hero.classList.add("hidden-hero");
+
+  /* OCULTAR TODAS */
+
+  const sections =
+  document.querySelectorAll(".hidden-section");
+
+  sections.forEach(section => {
+
+    section.classList.remove("active-section");
+  });
+
+  /* MOSTRAR SECCION */
+
+  document
+    .getElementById(id)
+    .classList.add("active-section");
+
+  /* CERRAR MENU */
+
+  cerrarMenu();
+
+  /* SCROLL ARRIBA */
+
+  window.scrollTo({
+
+    top:0,
+
+    behavior:"smooth"
+  });
 }
